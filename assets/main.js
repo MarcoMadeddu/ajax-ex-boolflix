bc4e676233c720e06f568e3e9d98e64fe7375d58
MarcoMadeddu <marcomade99@gmail.com>
2020-05-05 13:52:55 +0200

    still working

--- assets/main.js
+++ assets/main.js
@@ -8,17 +8,21 @@ $(document).ready(function () {
     var source = $("#film-template").html();
     var template = Handlebars.compile(source);
 
-    input.focus(); //.select()
+    input.focus();
 
     // S E A R C H  B Y  C L I C K //
     btnSearch.click(function(){
+        reset(list);  
         searchFilm(template, input ,list);
+        searchTvSerie(template, input ,list);
     });
 
      // S E A R C H  B Y  E N T E R  //
     input.keyup(function(e){
         if(e.which==13){
+        reset(list);
         searchFilm(template, input ,list);
+        searchTvSerie(template, input ,list);
         }
     });
 // E N D  D O C U M E N T  R E A D Y //
@@ -26,11 +30,15 @@ $(document).ready(function () {
 
 
 
-// F U N C T I O N S //
+// **F U N C T I O N S** //
+
+// R E S E T //
+function reset(list){
+    list.children().remove();
+}
 
 // S E A R C H F I L M //
 function searchFilm(template, from , list){
-    list.children().remove();
     var filmToSearch = from.val().trim();
     $.ajax({
         url: "https://api.themoviedb.org/3/search/movie",
@@ -43,14 +51,61 @@ function searchFilm(template, from , list){
         
         success: function (data) {
             var films = data.results;
+            if(films.length>0){
+                for(var i = 0; i < films.length; i++){
+                    var currentFilm = films[i];
+                    var value = {
+                        title: currentFilm.title,
+                        originalTitle: currentFilm.original_title,
+                        originaLanguage: getLanguage(currentFilm.original_language),
+                        averege: getvote(currentFilm.vote_average),
+                        type: "Film"
+                    };
+    
+                    
+                    var set = template(value);
+                    list.append(set);
+                    from.val("");
+                }
+            }else{
+                alert("NESSUN FILM");
+                input.select();
+            }
             
-            for(var i = 0; i < films.length; i++){
-                var currentFilm = films[i];
+            
+
+        },
+
+        error: function() {
+            console.log("Errore chiamata API film");
+            
+        }
+    });   
+}
+
+// S E A R C H  S E R I E //
+function searchTvSerie(template, from , list){
+    var serieToSearch = from.val().trim();
+    $.ajax({
+        url: "https://api.themoviedb.org/3/search/tv",
+        method: "GET",
+        data: {
+            api_key: "d095562fc608329d4fa8c044d034e379",
+            query: serieToSearch,
+            language: "it-IT"
+        },
+        
+        success: function (data) {
+            var series = data.results;
+            
+            for(var i = 0; i < series.length; i++){
+                var currentSerie = series[i];
                 var value = {
-                    title: currentFilm.title,
-                    originalTitle: currentFilm.original_title,
-                    originaLanguage: currentFilm.original_language,
-                    averege: getvote(currentFilm.vote_average)
+                    title: currentSerie.name,
+                    originalTitle: currentSerie.original_name,
+                    originaLanguage: getLanguage(currentSerie.original_language),
+                    averege: getvote(currentSerie.vote_average),
+                    type: "Serie Tv"
                 };
 
                 
@@ -62,25 +117,40 @@ function searchFilm(template, from , list){
         },
 
         error: function() {
-            console.log("oh no");
+            console.log("Errore chiamata API serie tv");
             
         }
-    });
-    
+    });   
 }
-
+// G E T  V O T E //
 function getvote(number){
     
-    if(number > 5){
-        number = 5;
-    }else{
-        number.toFixed(0);
-    }
+    var value = Math.floor(number/2);
+
     var stars = "";
-    var star = '<i class="fas fa-star"></i>'
-    for(var i = 0; i< number; i++){
-        stars+=star;
+    for(var i = 1; i<= 5; i++){
+        if(i <= value){
+            stars += '<i class="fas fa-star"></i>';
+        }else{
+            stars += '<i class="far fa-star"></i>';
+        }
     }
     return stars;
+}
+
+// G E T  L A N G U A G E //
+function getLanguage(type) {
+    var it = "./assets/img2/'it.svg'";
+    switch(type) {
+        case "it":
+          return '<img class="flag" src="img/it.svg" alt="it">';
+          break;
+        case "en":
+          return '<img class="flag" src="img/en.svg" alt="eng">';
+          break;
+        default:
+          return type;
+      }
+    
 }
 
